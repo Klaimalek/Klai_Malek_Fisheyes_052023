7a9f3e43c4bd61b1c612cc9aa43abe7ac0195202
Malek <absimalekmootez@gmail.com>
2023-05-14 23:30:34 +0200

    afficher les medias

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,3 +1,6 @@
+
+const profilMedia = document.querySelector("#profil__media");
+
 //récupérer l'ID du photographe pour charger les données des photographers
 
 function urlGetParams(url) {
@@ -6,10 +9,12 @@ function urlGetParams(url) {
   }
   const idPhotoghrapher = urlGetParams(document.location);
   console.log(idPhotoghrapher);
-
+   
+  
   const informationPhotographer = document.querySelectorAll('photograph-header');
-
-  async function setData() {
+  const galleryMedia = document.querySelectorAll('photographerMedia');
+  //obtenir les données
+  async function setDataProfil() {
     let response = await fetch("../data/photographers.json");
     if (!response.ok) {
       return "error";
@@ -19,17 +24,18 @@ function urlGetParams(url) {
     let photographer = data.photographers.find((element) => element.id == idPhotoghrapher);
     let media = data.media.filter((m) => m.photographerId == idPhotoghrapher);
   
-    setDataInHtml(photographer);
+    setDataElement(photographer,media);
+
     console.log(media);
   }
-  setData();
-
-  function setDataInHtml(photographer, media) {
-    setProfilHeader(photographer);
-   
+  setDataProfil();
+  // display les données poue le photographer
+  function setDataElement(photographer,media) {
+    setProfilPhotgrapher(photographer);
+     setMedia(media);
   }
   //Mettre info dans la presentation du photographe
-function setProfilHeader(photographer) {
+function  setProfilPhotgrapher(photographer) {
 
   document.getElementById("namePhotographer").innerText = photographer.name;
   document.getElementById(
@@ -41,3 +47,49 @@ function setProfilHeader(photographer) {
     "photographerImg"
   ).src = `assets/photographers/${photographer.portrait}`;
 }
+
+function setMedia(media) {
+  media.forEach((element) => {
+    let media = mediaFactory(element);
+    let li = media.createElement();
+    profilMedia.innerHTML += li;
+  });
+  listMedia = document.querySelectorAll("#profil__media li");
+}
+ 
+
+function mediaFactory(data) {
+  let type = data.video ? "video" : "image";
+
+  function createElement() {
+    let element;
+    if (type == "image") {
+      element = `<img src=assets/media/${data.photographerId}/${data.image} alt="${data.title}" data-id=${data.id}></img>`;
+    } else if (type == "video") {
+      element = `<video src=assets/media/${data.photographerId}/${data.video}#t=0.1 alt="${data.title}" data-id=${data.id} preload="metadata"></video>`;
+      //#t=0.1 et preload="metadata" pour que l'image de la video s'affiche sur safari
+    }
+
+    const li = `
+              <li class="media" data-date=${data.date} data-likes=${data.likes} data-title=${data.title}>
+                <a href="#" class="media__link" >
+                  ${element}
+                </a>
+                <div class="media__info">
+                  <p>${data.title}</p>
+                  <button class="like" >
+                    <p>${data.likes}</p>
+                    <img src="assets/icons/heart-vide.svg" id="coeurVide" alt="coeur-vide"/> 
+                    <img src="assets/icons/heart.svg" id="coeur" alt="coeur"/>   
+                  </button>
+                </div>
+              </li>
+            `;
+    return li;
+  }
+  return { type, createElement };
+}
+
+console.log("ok");
+
+
