77091753640de6956a9d38ec81b7b5b5d9ed4547
Malek <absimalekmootez@gmail.com>
2023-05-17 22:52:41 +0200

    lancher ke modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,7 +1,22 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+//Récupération des éléments
+
+const btnContact = document.getElementsByClassName('contact_button');
+const modal = document.getElementById("contact_modal");
+
+
+// launch modal event
+btnContact[0].addEventListener('click', launchModal);
+
+// launch modal form
+function launchModal() {
+    modal .style.display = 'block';
 }
+
+/*function displayModal() {
+    const modal = document.getElementById("contact_modal");
+     modal.style.display = "block";
+    
+}*/
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
